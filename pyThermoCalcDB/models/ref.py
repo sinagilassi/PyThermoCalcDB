9d09d1e0758678ce93b2cb846d57e257a8c5fa17
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2025-11-30 21:11:30 -0500

    Add CalcResult model to define calculated values and their properties

---
+++ pyThermoCalcDB/models/ref.py
@@ -0,0 +1,14 @@
+# import libs
+from typing import Optional, Dict, Any
+from pydantic import BaseModel, Field, ConfigDict
+
+# NOTE: calc result
+
+
+class CalcResult(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    value: float = Field(..., description="Calculated value")
+    unit: str = Field(..., description="Unit of the calculated value")
+    symbol: str = Field(...,
+                        description="Symbol representing the calculated property")
