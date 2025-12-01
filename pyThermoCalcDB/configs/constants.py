f8c2ae1512619cef2c3e2c401c21d8f71c6f1932
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2025-11-30 21:10:52 -0500

    Add constants for universal gas constant, reference temperature, and pressure in constants.py

--- pyThermoCalcDB/configs/constants.py
+++ pyThermoCalcDB/configs/constants.py
@@ -3,3 +3,9 @@
 # SECTION: PyThermoDBLink/PyThermoDB
 DATASOURCE = "datasource"
 EQUATIONSOURCE = "equationsource"
+
+
+# SECTION: constants
+R_J_molK = 8.314462618  # universal gas constant in J/mol.K
+T_ref_K = 298.15  # reference temperature in K
+P_ref_Pa = 101325.0  # reference pressure in Pa
