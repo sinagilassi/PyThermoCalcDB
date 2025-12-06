2f8b64f20a9b886fb3658fd253bb76c57e7d1883
sina.gilassi <sina.gilassi@gmail.com>
2025-12-06 00:06:19 -0500

    test

--- examples/custom-equation.py
+++ examples/custom-equation.py
@@ -5,6 +5,7 @@ from rich import print
 import pyThermoDB as ptdb
 import pyThermoLinkDB as ptdblink
 from pyThermoLinkDB.models import ModelSource
+from pyThermoLinkDB.thermo import mkeqs
 from pythermodb_settings.models import Component, ComponentRule, ComponentThermoDBSource, Temperature, Pressure
 # local
 
@@ -86,6 +87,9 @@ thermodb_rules: Dict[str, Dict[str, ComponentRule]] = {
     },
 }
 
+# ======================================================
+# SECTION: Build Model Source with thermodb rules
+# ======================================================
 model_source_: ModelSource = ptdblink.load_and_build_model_source(
     thermodb_sources=_component_thermodb,
     rules=thermodb_rules,
@@ -95,6 +99,17 @@ print(model_source_)
 # get data source and equation source
 datasource = model_source_.data_source
 equationsource = model_source_.equation_source
+
+# ======================================================
+# SECTION: Build Model Source without thermodb rules
+# ======================================================
+# ! original equation labels (False)
+# ! original equation labels (True)
+model_source_no_rules: ModelSource = ptdblink.load_and_build_model_source(
+    thermodb_sources=_component_thermodb,
+    original_equation_label=False
+)
+print(model_source_no_rules)
 
 # ------------------------------------------------
 # ! THERMODYNAMIC PROPERTIES
@@ -110,10 +125,36 @@ print(Cp_IG)
 # SECTION: MAKE EQUATION OBJECT
 # =======================================
 # NOTE: make equation object for vapor pressure
-VaPr_eq = mkeq(
-    prop_name='VaPr',
+CO2_eqs = mkeqs(
     component=CO2_comp,
     model_source=model_source_,
-    component_key='Name-State'
 )
-print(VaPr_eq)
+print(CO2_eqs)
+
+# vapor pressure
+if CO2_eqs:
+    VaPr_eq = CO2_eqs.eq('VaPr')
+    if VaPr_eq:
+        # args
+        args_ = VaPr_eq.args
+        print(args_)
+        arg_identifiers_ = VaPr_eq.arg_identifiers
+        print(arg_identifiers_)
+        arg_symbols_ = VaPr_eq.arg_symbols
+        print(arg_symbols_)
+        # inputs
+        inputs_ = VaPr_eq.inputs
+        print(inputs_)
+        # calculate
+        VaPr_val = VaPr_eq.fn(T=300.1)
+        print(VaPr_val)
+        VaPr_val = VaPr_eq.calc(T=300.1)
+        print(VaPr_val)
+
+# heat capacity
+    Cp_IG_eq = CO2_eqs.eq('Cp_IG')
+    if Cp_IG_eq:
+        Cp_IG_val = Cp_IG_eq.fn(T=300.1)
+        print(Cp_IG_val)
+        Cp_IG_val = Cp_IG_eq.calc(T=300.1)
+        print(Cp_IG_val)
