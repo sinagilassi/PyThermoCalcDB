99bd7e597128ef8e59875a83d0c6e3446cfcd593
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2026-01-01 17:56:57 -0500

    feat: Add ComponentEnthalpyChange to module exports for enhanced functionality

--- pyThermoCalcDB/models/__init__.py
+++ pyThermoCalcDB/models/__init__.py
@@ -4,10 +4,12 @@ from .ref import CalcResult
 from .component_ref import (
     ComponentEnthalpyOfFormation,
     ComponentGibbsEnergyOfFormation,
+    ComponentEnthalpyChange
 )
 
 __all__ = [
     'ComponentEnthalpyOfFormation',
     'ComponentGibbsEnergyOfFormation',
+    'ComponentEnthalpyChange',
     'CalcResult',
 ]
