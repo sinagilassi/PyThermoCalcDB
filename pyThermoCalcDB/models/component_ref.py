1b55eba433914ae3763e61bbff637b0eea4d6fcd
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2026-01-01 17:57:07 -0500

    feat: Add ComponentEnthalpyChange model for enthalpy change calculations with detailed attributes

--- pyThermoCalcDB/models/component_ref.py
+++ pyThermoCalcDB/models/component_ref.py
@@ -5,7 +5,52 @@ from pyThermoDB.core import TableEquation
 from pythermodb_settings.models import Temperature
 
 
+# NOTE: enthalpy change result model
+class ComponentEnthalpyChange(BaseModel):
+    '''
+    Component Enthalpy Change Result Model
+
+    Attributes
+    ----------
+    temperature_initial : Temperature
+        The initial temperature for the enthalpy change calculation.
+    temperature_final : Temperature
+        The final temperature for the enthalpy change calculation.
+    value : float
+        The enthalpy change value.
+    unit : str
+        The enthalpy change unit.
+    symbol : str
+        The enthalpy change symbol.
+    '''
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    temperature_initial: Temperature = Field(
+        ...,
+        description="The initial temperature for the enthalpy change calculation."
+    )
+
+    temperature_final: Temperature = Field(
+        ...,
+        description="The final temperature for the enthalpy change calculation."
+    )
+
+    value: float = Field(
+        ...,
+        description="The enthalpy change value."
+    )
+    unit: str = Field(
+        ...,
+        description="The enthalpy change unit."
+    )
+    symbol: str = Field(
+        ...,
+        description="The enthalpy change symbol."
+    )
+
 # NOTE: enthalpy of formation result model
+
+
 class ComponentEnthalpyOfFormation(BaseModel):
     '''
     Component Enthalpy of Formation Result Model
