84510910f3b63cc66779f26a837d30f74e3a28f7
sina.gilassi <sina.gilassi@gmail.com>
2026-01-02 16:42:44 -0500

    feat: Add missing symbols and units for liquid enthalpy and heat capacity

--- pyThermoCalcDB/configs/thermo_props.py
+++ pyThermoCalcDB/configs/thermo_props.py
@@ -3,17 +3,22 @@
 # SECTION: hsg properties
 # NOTE: symbols
 EnFo_IG_SYMBOL = 'EnFo_IG'  # enthalpy of formation symbol
+EnFo_LIQ_SYMBOL = 'EnFo_LIQ'  # enthalpy of formation liquid symbol
 Ent_STD_SYMBOL = 'Ent_STD'  # entropy of formation symbol
 GiEnFo_IG_SYMBOL = 'GiEnFo_IG'  # Gibbs free energy of formation symbol
 Cp_IG_SYMBOL = 'Cp_IG'  # ideal gas heat capacity symbol
+Cp_LIQ_SYMBOL = 'Cp_LIQ'  # liquid heat capacity symbol
 VaPr_SYMBOL = 'VaPr'  # vapor pressure symbol
 
 # NOTE: units
 EnFo_IG_UNIT = 'J/mol'  # enthalpy of formation unit
+EnFo_LIQ_UNIT = 'J/mol'  # enthalpy of formation liquid unit
 Ent_STD_UNIT = 'J/mol.K'  # entropy of formation unit
 GiEnFo_IG_UNIT = 'J/mol'  # Gibbs free energy of formation unit
 Cp_IG_UNIT = 'J/mol.K'  # ideal gas heat capacity unit
+Cp_LIQ_UNIT = 'J/mol.K'  # liquid heat capacity unit
 VaPr_UNIT = 'Pa'  # vapor pressure unit
 
 # NOTE: integration units
 Cp_IG_INTEGRATION_UNIT = 'J/mol'  # ideal gas heat capacity integration unit
+Cp_LIQ_INTEGRATION_UNIT = 'J/mol'  # liquid heat capacity integration unit
