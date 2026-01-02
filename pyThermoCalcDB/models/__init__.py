a7dcfc7ed3e5cf67eacd3a316634653163c92d30
sina.gilassi <sina.gilassi@gmail.com>
2026-01-02 16:43:23 -0500

    upd

--- pyThermoCalcDB/models/__init__.py
+++ pyThermoCalcDB/models/__init__.py
@@ -4,12 +4,14 @@ from .ref import CalcResult
 from .component_ref import (
     ComponentEnthalpyOfFormation,
     ComponentGibbsEnergyOfFormation,
-    ComponentEnthalpyChange
+    ComponentEnthalpyChange,
+    ComponentEntropyChange,
 )
 
 __all__ = [
     'ComponentEnthalpyOfFormation',
     'ComponentGibbsEnergyOfFormation',
     'ComponentEnthalpyChange',
+    'ComponentEntropyChange',
     'CalcResult',
 ]
