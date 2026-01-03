ff739bcfd0ba1b3d36f42121d1ea1c99fe998c69
sina.gilassi <sina.gilassi@gmail.com>
2026-01-02 19:38:13 -0500

    feat: Add missing symbols and units for enthalpy of formation, vaporization, and sublimation

--- pyThermoCalcDB/configs/thermo_props.py
+++ pyThermoCalcDB/configs/thermo_props.py
@@ -4,20 +4,26 @@
 # NOTE: symbols
 EnFo_IG_SYMBOL = 'EnFo_IG'  # enthalpy of formation symbol
 EnFo_LIQ_SYMBOL = 'EnFo_LIQ'  # enthalpy of formation liquid symbol
+EnFo_SOL_SYMBOL = 'EnFo_SOL'  # enthalpy of formation solid symbol
 Ent_STD_SYMBOL = 'Ent_STD'  # entropy of formation symbol
 GiEnFo_IG_SYMBOL = 'GiEnFo_IG'  # Gibbs free energy of formation symbol
 Cp_IG_SYMBOL = 'Cp_IG'  # ideal gas heat capacity symbol
 Cp_LIQ_SYMBOL = 'Cp_LIQ'  # liquid heat capacity symbol
 VaPr_SYMBOL = 'VaPr'  # vapor pressure symbol
+EnVap_SYMBOL = 'EnVap'  # enthalpy of vaporization symbol
+EnSub_SYMBOL = 'EnSub'  # enthalpy of sublimation symbol
 
 # NOTE: units
 EnFo_IG_UNIT = 'J/mol'  # enthalpy of formation unit
 EnFo_LIQ_UNIT = 'J/mol'  # enthalpy of formation liquid unit
+EnFo_SOL_UNIT = 'J/mol'  # enthalpy of formation solid unit
 Ent_STD_UNIT = 'J/mol.K'  # entropy of formation unit
 GiEnFo_IG_UNIT = 'J/mol'  # Gibbs free energy of formation unit
 Cp_IG_UNIT = 'J/mol.K'  # ideal gas heat capacity unit
 Cp_LIQ_UNIT = 'J/mol.K'  # liquid heat capacity unit
 VaPr_UNIT = 'Pa'  # vapor pressure unit
+EnVap_UNIT = 'J/mol'  # enthalpy of vaporization unit
+EnSub_UNIT = 'J/mol'  # enthalpy of sublimation unit
 
 # NOTE: integration units
 Cp_IG_INTEGRATION_UNIT = 'J/mol'  # ideal gas heat capacity integration unit
