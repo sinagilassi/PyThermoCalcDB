a54480a0f88a49b6bc12e63eabb3330ba4e6610d
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2026-01-07 21:40:59 -0500

    exp

--- examples/reaction/exp-1.py
+++ examples/reaction/exp-1.py
@@ -22,7 +22,7 @@ parent_dir = os.path.dirname(os.path.abspath(__file__))
 print(parent_dir)
 
 # NOTE: thermodb directory
-thermodb_dir = os.path.join(parent_dir, 'thermodb')
+thermodb_dir = os.path.join(parent_dir, '..', 'thermodb')
 print(thermodb_dir)
 
 # ! CO2
@@ -36,7 +36,10 @@ CO_thermodb_file = os.path.join(thermodb_dir, 'carbon monoxide-g.pkl')
 CH4_thermodb_file = os.path.join(thermodb_dir, 'methane-g.pkl')
 
 # ! H2O
-H2O_thermodb_file = os.path.join(thermodb_dir, 'water-g.pkl')
+H2O_thermodb_file = os.path.join(thermodb_dir, 'water-l.pkl')
+
+# ! O2
+O2_thermodb_file = os.path.join(thermodb_dir, 'oxygen-g.pkl')
 
 # =======================================
 # SECTION: COMPONENTS THERMODB SOURCE
@@ -90,37 +93,32 @@ H2O_component_thermodb: ComponentThermoDBSource = ComponentThermoDBSource(
     source=H2O_thermodb_file
 )
 
+# ! O2
+O2_comp = Component(
+    name='oxygen',
+    formula='O2',
+    state='g'
+)
+# ! component thermodb source
+O2_component_thermodb: ComponentThermoDBSource = ComponentThermoDBSource(
+    component=O2_comp,
+    source=O2_thermodb_file
+)
+
 # NOTE: component thermodb source
 _component_thermodb: list = [
     CO2_component_thermodb,
     CO_component_thermodb,
     CH4_component_thermodb,
-    H2O_component_thermodb
+    H2O_component_thermodb,
+    O2_component_thermodb,
 ]
 
 # =======================================
 # SECTION: BUILD THERMODB MODEL SOURCE
 # =======================================
-# update thermodb rule
-thermodb_rules: Dict[str, Dict[str, ComponentRule]] = {
-    'ALL_HIDDEN': {
-        'DATA': {
-            'critical-pressure': 'Pc',
-            'critical-temperature': 'Tc',
-            'acentric-factor': 'AcFa',
-            'Enthalpy-of-Formation': 'EnFo_IG',
-            'Gibbs-Energy-of-Formation': 'GiEnFo_IG',
-        },
-        'EQUATIONS': {
-            'CUSTOM-REF-1::vapor-pressure': 'VaPr',
-            'CUSTOM-REF-1::ideal-gas-heat-capacity': 'Cp_IG'
-        }
-    },
-}
-
 model_source_: ModelSource = ptdblink.load_and_build_model_source(
     thermodb_sources=_component_thermodb,
-    rules=thermodb_rules,
     original_equation_label=False
 )
 print(model_source_)
@@ -129,34 +127,38 @@ print(model_source_)
 datasource = model_source_.data_source
 equationsource = model_source_.equation_source
 
+# =======================================
 # SECTION: Components
+# =======================================
 CH4 = Component(
-    name="Methane",
+    name="methane",
     formula="CH4",
     state="g",
 )
 O2 = Component(
-    name="Oxygen",
+    name="oxygen",
     formula="O2",
     state="g",
 )
 CO2 = Component(
-    name="Carbon Dioxide",
+    name="carbon dioxide",
     formula="CO2",
     state="g",
 )
 H2O = Component(
-    name="Water",
+    name="water",
     formula="H2O",
     state="g",
 )
 
 components = [CH4, O2, CO2, H2O]
 
+# =======================================
 # SECTION: Define reaction
+# =======================================
 reaction = Reaction(
     name="Combustion of Methane",
-    reaction="CH4(g) + 2O2(g) -> CO2(g) + 2H2O(g)",
+    reaction="CH4(g) + 2O2(g) => CO2(g) + 2H2O(g)",
     components=components,
 )
 
