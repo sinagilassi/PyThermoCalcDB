12bf5433ea4a8f62b7e0f263f999c622392720ef
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2026-03-26 12:51:10 -0400

    feat: Add __init__.py for reactions module with initial exports

---
+++ pyThermoCalcDB/reactions/__init__.py
@@ -0,0 +1,19 @@
+# NOTE: reactions
+from .reactions import (
+    dH_rxn_STD,
+    dG_rxn_STD,
+    Keq_STD,
+    Keq_VH,
+    Keq,
+    Keq_VH_Shortcut
+)
+
+
+__all__ = [
+    "dH_rxn_STD",
+    "dG_rxn_STD",
+    "Keq_STD",
+    "Keq_VH",
+    "Keq",
+    "Keq_VH_Shortcut"
+]
