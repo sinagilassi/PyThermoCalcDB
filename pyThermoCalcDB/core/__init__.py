cd78e2c6e774bd236cebc39ce2010ff910b8853f
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2026-04-10 21:49:32 -0400

    feat: Import HSGProperties and update __all__ in core module

--- pyThermoCalcDB/core/__init__.py
+++ pyThermoCalcDB/core/__init__.py
@@ -1,2 +1,10 @@
 # NOTE: HSGReaction
 from .hsg_reaction import HSGReaction
+
+# NOTE: HSGProperties
+from .hsg_properties import HSGProperties
+
+__all__ = [
+    'HSGReaction',
+    'HSGProperties',
+]
