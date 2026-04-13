14f066cf62cb10a69b36f9ed2f5fc631809081a5
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2026-04-13 15:45:47 -0400

    feat: Import Temperature model and define reference temperature constant

--- pyThermoCalcDB/configs/constants.py
+++ pyThermoCalcDB/configs/constants.py
@@ -1,4 +1,5 @@
 # import libs
+from pythermodb_settings.models import Temperature
 
 # SECTION: PyThermoDBLink/PyThermoDB
 DATASOURCE = "datasource"
@@ -9,3 +10,9 @@ EQUATIONSOURCE = "equationsource"
 R_J_molK = 8.314462618  # universal gas constant in J/mol.K
 T_ref_K = 298.15  # reference temperature in K
 P_ref_Pa = 101325.0  # reference pressure in Pa
+
+# NOTE: reference temperature
+T_298_K: Temperature = Temperature(
+    value=298.15,
+    unit="K"
+)
