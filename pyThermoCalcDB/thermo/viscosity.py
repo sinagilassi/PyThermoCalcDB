a455a0446149d822fb6ed7196661ff5ae2042551
sina.gilassi <sina.gilassi@gmail.com>
2026-07-02 20:17:03 -0400

    feat: add viscosity module with logging and type imports

---
+++ pyThermoCalcDB/thermo/viscosity.py
@@ -0,0 +1,12 @@
+# import libs
+import logging
+from typing import Dict, Optional, Any
+from pythermodb_settings.models import (
+    Temperature,
+    Pressure,
+    CustomProp,
+)
+# locals
+
+# NOTE: set up logger
+logger = logging.getLogger(__name__)
