02756cb41bb23048edc595127d2a6e459d65ae2b
sina.gilassi <sina.gilassi@gmail.com>
2026-08-26 00:31:34 -0400

    feat: add molecular weight calculation example script

---
+++ examples/molecular-weight/mw-exp-1.py
@@ -0,0 +1,27 @@
+# import libs
+from rich import print
+from pythermocalcdb.thermo.molecular_weight import calculate_molecular_weight
+
+# NOTE: Compounds
+
+compounds = [
+    "Fe(OH)3",
+    "Ca3(PO4)2",
+    "CuSO4*5H2O",
+    "SO4{2-}",
+    "e{-}",
+    "H2O",
+    "C6H12O6",
+    "Fe{3+}",
+    "H{+}",
+    "OH{-}",
+]
+
+# NOTE: Calculate and print molecular weights
+for compound in compounds:
+    mw = calculate_molecular_weight(
+        compound,
+        include_electron_mass=True,
+        decimal_digits=3
+    )
+    print(f"Molecular weight of {compound}: {mw}")
