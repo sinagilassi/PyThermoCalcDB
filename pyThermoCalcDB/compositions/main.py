055c537c56125b02b2dd08e517a05a8c043bc7cd
sina.gilassi <sina.gilassi@gmail.com>
2026-09-01 23:56:25 -0400

    feat: remove main.py and create utils.py for composition utilities

--- pyThermoCalcDB/compositions/main.py
+++
@@ -1,9 +0,0 @@
-# import libs
-import logging
-from typing import List, Dict
-from pythermodb_settings.models import Component, ComponentKey
-from pythermodb_settings.utils import find_component_by_id, set_component_id, measure_time
-# locals
-
-# NOTE: logger setup
-logger = logging.getLogger(__name__)
