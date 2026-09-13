0848517eed65e03aa1ab15d8bc9871e7131a9096
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2026-09-12 23:45:18 -0400

    feat: update __init__.py to include additional functions for ideal mixture density and entropy calculations

--- pyThermoCalcDB/mixtures/__init__.py
+++ pyThermoCalcDB/mixtures/__init__.py
@@ -1,5 +1,9 @@
 # NOTE: ideal mixture density
-from .density import calc_ideal_mixture_density
+from .density import (
+    calc_ideal_mixture_density_from_sequence,
+    calc_ideal_mixture_density_from_mapping,
+    calc_ideal_mixture_density_from_props,
+)
 
 # NOTE: ideal mixture heat capacity
 from .heat_capacity import calc_ideal_mixture_heat_capacity
@@ -16,7 +20,9 @@ from .molecular_weight import (
 
 # NOTE: ideal mixing entropy
 from .entropy import (
-    calc_ideal_molar_entropy_of_mixing,
+    calc_ideal_entropy_of_mixing_from_sequence,
+    calc_ideal_entropy_of_mixing_from_mapping,
+    calc_ideal_entropy_of_mixing_from_props,
     calc_ideal_entropy_of_mixing,
 )
 
@@ -42,16 +48,27 @@ from .volume_fraction import (
 
 
 __all__ = [
-    "calc_ideal_mixture_density",
+    # density
+    "calc_ideal_mixture_density_from_sequence",
+    "calc_ideal_mixture_density_from_mapping",
+    "calc_ideal_mixture_density_from_props",
+    # heat capacity
     "calc_ideal_mixture_heat_capacity",
+    # molecular weight
     "calc_mixture_molecular_weight_from_mole_fractions",
     "calc_mixture_molecular_weight_from_mass_fractions",
     "calc_mixture_molecular_weight_1",
     "calc_mixture_molecular_weight_2",
     "calc_mixture_molecular_weight_from_mass_fractions_1",
     "calc_mixture_molecular_weight_from_mass_fractions_2",
-    "calc_ideal_molar_entropy_of_mixing",
+    # entropy
+    "calc_ideal_entropy_of_mixing_from_mapping",
+    "calc_ideal_entropy_of_mixing_from_props",
     "calc_ideal_entropy_of_mixing",
+    "calc_ideal_entropy_of_mixing_from_sequence",
+    "calc_ideal_entropy_of_mixing_from_mapping",
+    "calc_ideal_entropy_of_mixing_from_props",
+    # gibbs energy
     "calc_ideal_molar_gibbs_energy_of_mixing",
     "calc_ideal_molar_gibbs_energy_of_mixing_from_sequence",
     "calc_ideal_molar_gibbs_energy_of_mixing_from_mapping",
@@ -62,8 +79,7 @@ __all__ = [
     "calc_ideal_gibbs_energy_of_mixing_from_sequence",
     "calc_ideal_gibbs_energy_of_mixing_from_mapping",
     "calc_ideal_gibbs_energy_of_mixing_from_props",
+    # volume fraction
     "calc_volume_fractions",
     "mass_fraction_to_volume_fraction",
 ]
-
-
